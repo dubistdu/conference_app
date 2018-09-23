44e4831898613bf993b45c3d94b76c3ab9c6a0ea
mama frantz <hyunfrantz@gmail.com>
2018-09-23 01:26:34 -0400

    Create a new endpoint to show the list of speakers, as JSON

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/activerecord'
 
+#  won't ba albe to create speaker record without requiring relative file - speaker
 require_relative 'models/speaker'
 
 class ConferenceApp < Sinatra::Base
@@ -16,4 +17,11 @@ class ConferenceApp < Sinatra::Base
     message = { success: true, message: 'hello'}
     json message
   end
+
+  # Create a new endpoint to show the list of speakers, as JSON.
+  get '/speakers' do
+    @speakers = Speaker.all
+    json @speakers
+  end
+
 end
